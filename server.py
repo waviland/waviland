4d8463ab5ea8ca6d15c2ce7145b6a98ee78d591a
waviland <serverwavilandmc@gmail.com>
2024-10-20 01:55:01 +0000

    Mundo guardado 2024-10-20

--- server.py
+++ server.py
@@ -30,7 +30,7 @@
 
 
 
-# No toques nada de aquí para abajo, puedes dañarlo
+# No toques nada de aquí para abajo, puedes dañarlo1
 import requests,os,base64,glob,time
 if os.path.exists("servidor.py"):
 	os.remove("servidor.py")
